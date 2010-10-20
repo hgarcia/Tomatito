f5ef09bbe10e54b2b828c9bafd5b723d921a8852
hgarcia <hernan@dynamicprogrammer.com>
2010-10-19 23:11:44 -0400

    Tracking interruptions and pomodoros for the run

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,9 +11,11 @@ namespace Tomatito
     {
         private DispatcherTimer _dispatcherTimer;
         private TimeSpan _pomodoro;
-        private readonly SolidColorBrush _green = new SolidColorBrush(Color.FromArgb(255, 7, 163, 20));
-        private readonly SolidColorBrush _red = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
         private readonly TimeSpan _vibrateDuration = new TimeSpan(0,0,3);
+        private const string Stop = "Stop";
+        private const string Start = "Start";
+        private const string Working = "Working...";
+        private const string OnBreak = "On break";
         // Constructor
         public MainPage()
         {
@@ -24,31 +26,21 @@ namespace Tomatito
         private void resetPomodoro()
         {
             if (_dispatcherTimer != null && _dispatcherTimer.IsEnabled) _dispatcherTimer.Stop();
-            button1.Content = "Start";
-            button1.Background = _green;
-            textBlock1.Foreground = _green;
-            _pomodoro = new TimeSpan(0,25,0);
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            if (((Button) sender).Content.ToString().ToLower() == "start"){
-            startPomodoro(PomodoroTimerClick);
-            }
-             else if (((Button)sender).Content.ToString().ToLower().Contains("stop"))
-                resetPomodoro();
+            button1.Content = Start;
+            _pomodoro = new TimeSpan(0,0,5);
+            displayTime();
         }
 
         private void startPomodoro(EventHandler handler)
         {
-            button1.Content = "Stop";
-            button1.Background = _red;
+            button1.Content = Stop;
+            textBlock2.Text = Working;
             _dispatcherTimer = new DispatcherTimer { Interval = Interval };
             _dispatcherTimer.Tick += handler;
             _dispatcherTimer.Start();
         }
 
-        private TimeSpan Interval
+        private static TimeSpan Interval
         {
             get { 
                 return new TimeSpan(0, 0, 1); 
@@ -58,8 +50,16 @@ namespace Tomatito
         private void PomodoroTimerClick(object sender, EventArgs e)
         {
             _pomodoro = _pomodoro.Subtract(Interval);
-            textBlock1.Text = _pomodoro.Minutes + ":" + _pomodoro.Seconds;
+            displayTime();
             if (_pomodoro.TotalSeconds == 0) startBreak();
+        }
+
+        private void displayTime()
+        {
+            var minutes = _pomodoro.Minutes < 10 ? "0" + _pomodoro.Minutes : _pomodoro.Minutes.ToString();
+            var seconds = _pomodoro.Seconds < 10 ? "0" + _pomodoro.Seconds : _pomodoro.Seconds.ToString();
+
+            textBlock1.Text = minutes + ":" + seconds;
         }
 
         private void startBreak()
@@ -67,21 +67,30 @@ namespace Tomatito
             soundAlarm();
             _dispatcherTimer.Stop();
             startPomodoro(BreakTimerClick);
-            button1.Content = "Stop";
-            button1.Background = _red;
-            textBlock1.Foreground = _red;
+            textBlock1.Foreground = new SolidColorBrush(Colors.Yellow);
+            textBlock2.Text = OnBreak;
+            button1.Content = Stop;
         }
 
         private void soundAlarm()
         {
-            
             Microsoft.Devices.VibrateController.Default.Start(_vibrateDuration);
         }
 
         private void BreakTimerClick(object sender, EventArgs e)
         {
             _pomodoro = _pomodoro.Add(Interval);
-            textBlock1.Text = _pomodoro.Minutes + ":" + _pomodoro.Seconds;                
+            displayTime();              
+        }
+
+        private void CounterButtonClickHandler(object sender, RoutedEventArgs e)
+        {
+            if (((Button)sender).Content.ToString().ToLower() == "start")
+            {
+                startPomodoro(PomodoroTimerClick);
+            }
+            else if (((Button)sender).Content.ToString().ToLower().Contains("stop"))
+                resetPomodoro();
         }
     }
 }
