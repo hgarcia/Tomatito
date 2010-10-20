3bf241fc096c1c9a78caf18157830c3fa302e3ff
hgarcia <hernan@dynamicprogrammer.com>
2010-10-20 00:02:46 -0400

    Tracking interruptions and pomodoros for the run

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,19 +3,22 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Threading;
+using Microsoft.Devices;
 using Microsoft.Phone.Controls;
 
 namespace Tomatito
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        private DispatcherTimer _dispatcherTimer;
-        private TimeSpan _pomodoro;
-        private readonly TimeSpan _vibrateDuration = new TimeSpan(0,0,3);
         private const string Stop = "Stop";
         private const string Start = "Start";
         private const string Working = "Working...";
         private const string OnBreak = "On break";
+        private readonly TimeSpan _breakSpan = new TimeSpan(0, 5, 0);
+        private readonly TimeSpan _pomodoroSpan = new TimeSpan(0, 25, 0);
+        private readonly TimeSpan _vibrateDuration = new TimeSpan(0, 0, 3);
+        private DispatcherTimer _dispatcherTimer;
+        private TimeSpan _pomodoro;
         // Constructor
         public MainPage()
         {
@@ -23,28 +26,39 @@ namespace Tomatito
             resetPomodoro();
         }
 
+        private static TimeSpan Interval
+        {
+            get { return new TimeSpan(0, 0, 1); }
+        }
+
+        protected int PomodoroCount { get; set; }
+        protected int Interruptions { get; set; }
+        protected double BreakDurationInSeconds
+        {
+            get
+            {
+                if (PomodoroCount % 4 == 0) return _breakSpan.TotalSeconds*3;
+                return _breakSpan.TotalSeconds;
+            }
+        }
+
         private void resetPomodoro()
         {
             if (_dispatcherTimer != null && _dispatcherTimer.IsEnabled) _dispatcherTimer.Stop();
-            button1.Content = Start;
-            _pomodoro = new TimeSpan(0,0,5);
+            pomodoroButton.Content = Start;
+            statusLabel.Text = string.Empty;
+            _pomodoro = _pomodoroSpan;
+            clockDisplay.Foreground = new SolidColorBrush(Colors.Green);
             displayTime();
         }
 
         private void startPomodoro(EventHandler handler)
         {
-            button1.Content = Stop;
-            textBlock2.Text = Working;
-            _dispatcherTimer = new DispatcherTimer { Interval = Interval };
+            pomodoroButton.Content = Stop;
+            statusLabel.Text = Working;
+            _dispatcherTimer = new DispatcherTimer {Interval = Interval};
             _dispatcherTimer.Tick += handler;
             _dispatcherTimer.Start();
-        }
-
-        private static TimeSpan Interval
-        {
-            get { 
-                return new TimeSpan(0, 0, 1); 
-            }
         }
 
         private void PomodoroTimerClick(object sender, EventArgs e)
@@ -56,41 +70,63 @@ namespace Tomatito
 
         private void displayTime()
         {
-            var minutes = _pomodoro.Minutes < 10 ? "0" + _pomodoro.Minutes : _pomodoro.Minutes.ToString();
-            var seconds = _pomodoro.Seconds < 10 ? "0" + _pomodoro.Seconds : _pomodoro.Seconds.ToString();
+            string minutes = _pomodoro.Minutes < 10 ? "0" + _pomodoro.Minutes : _pomodoro.Minutes.ToString();
+            string seconds = _pomodoro.Seconds < 10 ? "0" + _pomodoro.Seconds : _pomodoro.Seconds.ToString();
 
-            textBlock1.Text = minutes + ":" + seconds;
+            clockDisplay.Text = minutes + ":" + seconds;
         }
 
         private void startBreak()
         {
+            countPomodoro();
             soundAlarm();
             _dispatcherTimer.Stop();
             startPomodoro(BreakTimerClick);
-            textBlock1.Foreground = new SolidColorBrush(Colors.Yellow);
-            textBlock2.Text = OnBreak;
-            button1.Content = Stop;
+            clockDisplay.Foreground = new SolidColorBrush(Colors.Yellow);
+            statusLabel.Text = OnBreak;
+            pomodoroButton.Content = Stop;
+        }
+
+        private void countPomodoro()
+        {
+            PomodoroCount++;
+            pomodorosCount.Text = PomodoroCount.ToString();
         }
 
         private void soundAlarm()
         {
-            Microsoft.Devices.VibrateController.Default.Start(_vibrateDuration);
+            VibrateController.Default.Start(_vibrateDuration);
         }
 
         private void BreakTimerClick(object sender, EventArgs e)
         {
             _pomodoro = _pomodoro.Add(Interval);
-            displayTime();              
+            displayTime();
+            if (_pomodoro.TotalSeconds == BreakDurationInSeconds) resetPomodoro();
         }
 
         private void CounterButtonClickHandler(object sender, RoutedEventArgs e)
         {
-            if (((Button)sender).Content.ToString().ToLower() == "start")
+            switch (((Button) sender).Content.ToString())
             {
-                startPomodoro(PomodoroTimerClick);
+                case Start:
+                    startPomodoro(PomodoroTimerClick);
+                    break;
+                case Stop:
+                    resetPomodoro();
+                    break;
             }
-            else if (((Button)sender).Content.ToString().ToLower().Contains("stop"))
-                resetPomodoro();
+        }
+
+        private void addInterruptionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            countInterruptions();
+        }
+
+        private void countInterruptions()
+        {
+            Interruptions++;
+            interruptionsCount.Text = Interruptions.ToString();
         }
     }
 }
